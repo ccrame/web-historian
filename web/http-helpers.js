8d868504fede6511f891393ba075986646a7024c
Charles Crame <ccrame@gmail.com>
2015-07-23 19:15:40 -0700

    working helpers

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -1,6 +1,7 @@
 var path = require('path');
 var fs = require('fs');
 var archive = require('../helpers/archive-helpers');
+var helpers = require("./http-helpers.js");
 
 exports.headers = headers = {
   "access-control-allow-origin": "*",
@@ -13,7 +14,20 @@ exports.headers = headers = {
 exports.serveAssets = function(res, asset, callback) {
   // Write some code here that helps serve up your static files!
   // (Static files are things like html (yours or archived from others...), css, or anything that doesn't change often.)
+  fs.readFile(asset, function(error,content){
+    //console.log('content for ' + asset + content);
+        if(!error){
+          console.log('rendering html');
+          callback(content);
+        } else {
+          console.log('error!');
+          //res.writeHead(404, helpers.headers);
+          res.end();
+          //throw error;
+        }
+      });
 };
+
 
 
 
