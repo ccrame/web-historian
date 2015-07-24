c3d4cc66feb43d5fdee269ed464abc9a6967b5f7
Charles Crame <ccrame@gmail.com>
2015-07-23 19:16:58 -0700

    working function that retrieves html contents

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,24 @@
+var http = require('http');
+var archives = require('../helpers/archive-helpers');
+var fs = require('fs');
+var Q = require('q');
+
 // eventually, you'll have some code here that uses the code in `archive-helpers.js`
 // to actually download the urls you want to download.
+module.exports = function(url, cb){
+  //var deferred = Q.defer();
+  var temp = '';
+  console.log(url);
+  http.get("http://"+ url, function(res){
+      
+      res.on('data',function(chunk){
+        temp += chunk.toString();
+      });
+      res.on('end',function(){
+        cb(temp);
+      });
+  });
+  //console.log(deferred.promise);
+  //return deferred.promise;
+};
+
